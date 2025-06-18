b1aa1314f117ae407087d1fe881fc86ffd72993c
Muqtarali <162393700+Muqtarali@users.noreply.github.com>
2025-06-18 23:15:35 +0530

    Delete libs directory

--- libs/dbConnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI, {
-    dbName: 'finly-db',
-    bufferCommands: false,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
